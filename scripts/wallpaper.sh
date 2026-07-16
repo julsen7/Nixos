7a5dd9893b486e72e67ce33ea1779c71a5176961
julsen7 <263753131+julsen7@users.noreply.github.com>
2026-07-16 13:58:47 +0200

    changes

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -31,5 +31,15 @@ if [[ -n "$SELECTED_NAME" ]]; then
 
     matugen image "$WALLPAPER_PATH" --source-color-index 0 >/dev/null 2>&1
 
+    COLORFUL_THEME_DIR="$HOME/themes/colorful"
+
+    cp -f "$COLORFUL_THEME_DIR/dunstrc" "$HOME/.config/dunst/dunstrc"
+    cp -f "$COLORFUL_THEME_DIR/hypr.lua" "$HOME/.config/hypr/colors.lua"
+    cp -f "$COLORFUL_THEME_DIR/hypr.conf" "$HOME/.config/hypr/colors.conf"
+    cp -f "$COLORFUL_THEME_DIR/kitty.conf" "$HOME/.config/kitty/current-theme.conf"
+    sed -i 's/Theme=com.obsproject.Yami.Acri/Theme=com.obsproject.colorful/g' "$HOME/.config/obs-studio/user.ini"
+    cp -f "$COLORFUL_THEME_DIR/rofi.rasi" "$HOME/.config/rofi/colors.rasi"
+    cp -f "$COLORFUL_THEME_DIR/waybar.css" "$HOME/.config/waybar/colors.css"
+
     dunstify "Wallpaper" "Set $SELECTED_NAME as wallpaper" -i "$WALLPAPER_PATH"
 fi
