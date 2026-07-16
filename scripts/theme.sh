2ec52476c3d4ebfb42347840e1b8ef1698f9a1ce
julsen7 <263753131+julsen7@users.noreply.github.com>
2026-07-16 13:21:17 +0200

    changes

--- scripts/theme.sh
+++ scripts/theme.sh
@@ -6,7 +6,7 @@ BLACKWHITE_ICON="$HOME/scripts/blackwhite.jpg"
 COLOR_ICON="$HOME/scripts/colorful.jpg"
 
 themes=(
-    "BlackWhite\0icon\x1f${$BLACKWHITE_ICON}"
+    "BlackWhite\0icon\x1f${BLACKWHITE_ICON}"
     "Colorful\0icon\x1f${COLOR_ICON}"
 )
 
@@ -17,28 +17,35 @@ if [[ -n "$SELECTED_THEME" ]]; then
     if [[ "$SELECTED_THEME" == "BlackWhite" ]]; then
         BLACKWHITE_THEME_DIR="$HOME/themes/blackwhite"
 
-        ln -sf "$BLACKWHITE_THEME_DIR/dunstrc" "$HOME/.config/dunst/dunstrc"
-        ln -sf "$BLACKWHITE_THEME_DIR/hypr.lua" "$HOME/.config/hypr/colors.lua"
-        ln -sf "$BLACKWHITE_THEME_DIR/hypr.conf" "$HOME/.config/hypr/colors.conf"
-        ln -sf "$BLACKWHITE_THEME_DIR/kitty.conf" "$HOME/.config/kitty/current-theme.conf"
+        cp -f "$BLACKWHITE_THEME_DIR/dunstrc" "$HOME/.config/dunst/dunstrc"
+        cp -f "$BLACKWHITE_THEME_DIR/hypr.lua" "$HOME/.config/hypr/colors.lua"
+        cp -f "$BLACKWHITE_THEME_DIR/hypr.conf" "$HOME/.config/hypr/colors.conf"
+        cp -f "$BLACKWHITE_THEME_DIR/kitty.conf" "$HOME/.config/kitty/current-theme.conf"
         sed -i 's/Theme=com.obsproject.colorful/Theme=com.obsproject.Yami.Acri/g' "$HOME/.config/obs-studio/user.ini"
-        ln -sf "$BLACKWHITE_THEME_DIR/rofi.rasi" "$HOME/.config/rofi/colors.rasi"
-        ln -sf "$BLACKWHITE_THEME_DIR/waybar.css" "$HOME/.config/waybar/colors.css"
+        cp -f "$BLACKWHITE_THEME_DIR/rofi.rasi" "$HOME/.config/rofi/colors.rasi"
+        cp -f "$BLACKWHITE_THEME_DIR/waybar.css" "$HOME/.config/waybar/colors.css"
 
         dunstify "Theme Selector" "$SELECTED_THEME theme selected" -i "$BLACKWHITE_ICON"
     elif [[ "$SELECTED_THEME" == "Colorful" ]]; then
         COLORFUL_THEME_DIR="$HOME/themes/colorful"
 
-        ln -sf "$COLORFUL_THEME_DIR/dunstrc" "$HOME/.config/dunst/dunstrc"
-        ln -sf "$COLORFUL_THEME_DIR/hypr.lua" "$HOME/.config/hypr/colors.lua"
-        ln -sf "$COLORFUL_THEME_DIR/hypr.conf" "$HOME/.config/hypr/colors.conf"
-        ln -sf "$COLORFUL_THEME_DIR/kitty.conf" "$HOME/.config/kitty/current-theme.conf"
+        cp -f "$COLORFUL_THEME_DIR/dunstrc" "$HOME/.config/dunst/dunstrc"
+        cp -f "$COLORFUL_THEME_DIR/hypr.lua" "$HOME/.config/hypr/colors.lua"
+        cp -f "$COLORFUL_THEME_DIR/hypr.conf" "$HOME/.config/hypr/colors.conf"
+        cp -f "$COLORFUL_THEME_DIR/kitty.conf" "$HOME/.config/kitty/current-theme.conf"
         sed -i 's/Theme=com.obsproject.Yami.Acri/Theme=com.obsproject.colorful/g' "$HOME/.config/obs-studio/user.ini"
-        ln -sf "$COLORFUL_THEME_DIR/rofi.rasi" "$HOME/.config/rofi/colors.rasi"
-        ln -sf "$COLORFUL_THEME_DIR/waybar.css" "$HOME/.config/waybar/colors.css"
+        cp -f "$COLORFUL_THEME_DIR/rofi.rasi" "$HOME/.config/rofi/colors.rasi"
+        cp -f "$COLORFUL_THEME_DIR/waybar.css" "$HOME/.config/waybar/colors.css"
 
         dunstify "Theme Selector" "$SELECTED_THEME theme selected" -i "$COLOR_ICON"
     fi
+
+    chmod +w "$HOME/.config/dunst/dunstrc" \
+             "$HOME/.config/hypr/colors.lua" \
+             "$HOME/.config/hypr/colors.conf" \
+             "$HOME/.config/kitty/current-theme.conf" \
+             "$HOME/.config/rofi/colors.rasi" \
+             "$HOME/.config/waybar/colors.css"
 
     dunstctl reload
     kitty +kitten themes --reload-in=all
